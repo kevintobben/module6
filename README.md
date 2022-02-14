b8c542a40bebacd968779e93e40ae2de5a721595
Kevin Tobben <kevintobben@outlook.com>
2022-02-14 11:35:33 +0100

    new

--- README.md
+++
@@ -1,3 +0,0 @@
-# module6
-Irritante website / One pager
-hihihihi
---
+++ js.js
@@ -0,0 +1,28 @@
+let text = "Sip News is built by Product Hunt and its awesome community.";
+let result = text.link("https://www.youtube.com/watch?v=ZZ5LpwO-An4&t=55s");
+document.getElementById("ytlink").innerHTML = result;
+
+function myFunction() {
+    var txt;
+    if (confirm("KLIK OP EEN KNOPPIE!")) {
+        txt = "WOW JE HEBT OP OK GEKLIKT, GROTE JONGEN BEN JE OOK!";
+    } else {
+        txt = "OP ANNULEREN GEKLIKT, ANNULEER JEZELF MAAR!";
+    }
+    document.getElementById("demo").innerHTML = txt;
+    if (x > 10) {
+        var snd = new Audio("/MLG music 10 hours (64 kbps).mp3 ");
+        snd.play();
+        alert("Thank you!");
+    }
+}
+
+function yourlink() {
+
+    var locs = ['http://buzzybuzz.biz/', 'https://www.omfgdogs.com/#'] //https://www.omfgdogs.com/#
+
+    for (let i = 0; i < locs.length; i++) {
+        window.open(locs[i])
+    }
+
+}
