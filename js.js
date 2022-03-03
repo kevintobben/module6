7c41be50df5ded169396df5398ffcd19e1464f9b
luukvdn <luukarend@icloud.com>
2022-03-03 13:13:46 +0100

    bob

--- js.js
+++ js.js
@@ -30,9 +30,10 @@ function yourlink() {
 
 var object = document.getElementById('beker');
 
-object.onclick=function(){
-    var x = Math.floor(Math.random()*300);
-    var y = Math.floor(Math.random()*300);
-    object.style.top = x + 'px';
-    object.style.left = y + 'px';
-}
+object.onmouseover=function(){
+  var x = Math.floor(Math.random()*713);
+  var y = Math.floor(Math.random()*903);
+  object.style.top = x + 'px';
+  object.style.left = y + 'px';
+};
+
