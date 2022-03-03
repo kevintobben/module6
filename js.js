f5da17263b2866d2fe16a82c92b5f0f729da4624
Kevin Tobben <kevintobben@outlook.com>
2022-03-03 13:35:48 +0100

    laatste

--- js.js
+++ js.js
@@ -30,10 +30,57 @@ function yourlink() {
 
 var object = document.getElementById('beker');
 
-object.onmouseover=function(){
-  var x = Math.floor(Math.random()*713);
-  var y = Math.floor(Math.random()*903);
-  object.style.top = x + 'px';
-  object.style.left = y + 'px';
+object.onmouseover = function() {
+    var x = Math.floor(Math.random() * 713);
+    var y = Math.floor(Math.random() * 903);
+    object.style.top = x + 'px';
+    object.style.left = y + 'px';
 };
+//dvd
+const section = document.querySelector("section");
+const logo = document.querySelector(".logo");
+const FPS = 600;
+section.style.height = window.innerHeight + "px";
+section.style.width = window.innerWidth + "px";
 
+// logo bewegen
+let xPosition = 25;
+let yPosition = 25;
+let xSpeed = 25;
+let ySpeed = 25;
+
+function update() {
+    logo.style.left = xPosition + "px";
+    logo.style.top = yPosition + "px";
+}
+
+setInterval(() => {
+    if (xPosition + logo.clientWidth >= window.innerWidth || xPosition <= 0) {
+        xSpeed = -xSpeed;
+        logo.style.fill = randomColor();
+    }
+    if (yPosition + logo.clientHeight >= window.innerHeight || yPosition <= 0) {
+        ySpeed = -ySpeed;
+        logo.style.fill = randomColor();
+    }
+
+    xPosition += xSpeed;
+    yPosition += ySpeed;
+    update();
+}, 1000 / FPS);
+
+function randomColor() {
+    let color = "#";
+    color += Math.random().toString(16).slice(2, 8).toUpperCase();
+
+    return color;
+}
+console.log(randomColor());
+
+window.addEventListener("resize", () => {
+    xPosition = 10;
+    yPosition = 10;
+
+    section.style.height = window.innerHeight + "px";
+    section.style.width = window.innerWidth + "px";
+});
