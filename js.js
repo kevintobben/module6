976286ab4561119708dfb5e66ec0f5c31da239c3
luukvdn <luukarend@icloud.com>
2022-03-03 11:17:01 +0100

    det

--- js.js
+++ js.js
@@ -26,3 +26,13 @@ function yourlink() {
     }
     //hi
 }
+
+
+var object = document.getElementById('beker');
+
+object.onclick=function(){
+    var x = Math.floor(Math.random()*300);
+    var y = Math.floor(Math.random()*300);
+    object.style.top = x + 'px';
+    object.style.left = y + 'px';
+}bbbb
